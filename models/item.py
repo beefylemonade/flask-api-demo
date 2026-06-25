5814e49f1747c11db4bae19ed24d00630d0fea1a
Beefy Lemonade <beefylemonade@gmail.com>
2026-06-25 16:56:58 +0700

    fixed typo with item_tags

--- models/item.py
+++ models/item.py
@@ -5,12 +5,12 @@ class ItemModel(db.Model):
     __tablename__ = "items"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=False, nullable=False)
+    name = db.Column(db.String(80), unique=True, nullable=False)
     price = db.Column(db.Float(precision=2), unique=False, nullable=False)
+
     store_id = db.Column(
         db.Integer, db.ForeignKey("stores.id"), unique=False, nullable=False
     )
+    store = db.relationship("StoreModel", back_populates="items")
 
-    store = db.relationship("StoreModel", back_populates="items") # Just to easily access, to refer to the store asociated to it
-    tags = db.relationship("TagModel", back_populate="items", secondary="items_tags")
-
+    tags = db.relationship("TagModel", back_populates="items", secondary="items_tags")
